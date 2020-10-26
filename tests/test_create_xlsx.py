201b37b5ea735ec4fc621694b0618e2c0ad24b18
mccalluc <mccalluc@users.noreply.github.com>
2020-10-25 22:45:25 -0400

    fixtures directory and tests

--- tests/test_create_xlsx.py
+++ tests/test_create_xlsx.py
@@ -8,6 +8,18 @@ from yaml import safe_load
 from create_xlsx import create_xlsx
 
 
+def assert_xmls_match(xlsx_path, zip_path):
+    xml_path = zipfile.Path(xlsx_path, zip_path)
+    dom = xml.dom.minidom.parseString(xml_path.read_text())
+    pretty_xml = dom.toprettyxml()
+    pretty_xml_fixture_path = (
+        Path(__file__).parent / 'fixtures/output-unzipped' / zip_path
+    )
+    assert pretty_xml.strip() == \
+        pretty_xml_fixture_path.read_text().strip(), \
+        f'Update "{pretty_xml_fixture_path.name}" fixture?'
+
+
 def test_create_xlsx():
     schema_path = Path(__file__).parent / 'fixtures/schema.yaml'
     schema = safe_load(schema_path.read_text())
@@ -15,11 +27,6 @@ def test_create_xlsx():
         xlsx_path = Path(temp_dir_name) / 'template.xlsx'
         create_xlsx(schema, xlsx_path)
 
-        sheet_path = zipfile.Path(xlsx_path, 'xl/worksheets/sheet1.xml')
-        dom = xml.dom.minidom.parseString(sheet_path.read_text())
-        pretty_xml = dom.toprettyxml()
-        pretty_xml_fixture_path = Path(__file__).parent / 'fixtures/sheet1.xml'
-        assert pretty_xml.strip() == \
-            pretty_xml_fixture_path.read_text().strip()
-        # zipfile.Path(xlsx_path, 'xl/worksheets/sheet1.xml')
-        # zipfile.Path(xlsx_path, 'xl/worksheets/sheet1.xml')
+        assert_xmls_match(xlsx_path, 'xl/worksheets/sheet1.xml')
+        assert_xmls_match(xlsx_path, 'xl/sharedStrings.xml')
+        assert_xmls_match(xlsx_path, 'xl/comments1.xml')
