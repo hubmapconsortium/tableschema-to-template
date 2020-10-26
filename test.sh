75c95f9990fe290bfe4bfb140ad7197ac17ad922
mccalluc <mccalluc@users.noreply.github.com>
2020-10-25 23:18:25 -0400

    CLI test

--- test.sh
+++ test.sh
@@ -17,3 +17,24 @@ end flake8
 start pytest
 PYTHONPATH="${PYTHONPATH}:src" pytest -vv
 end pytest
+
+start cli
+# Make tempdir and cleanup afterwards.
+OLD=`mktemp -d`
+NEW=`mktemp -d`
+
+src/ts2xl.py \
+  --input_schema tests/fixtures/schema.yaml \
+  --output_dir $NEW
+unzip -q $NEW/template.xlsx -d $NEW
+
+cp tests/fixtures/template.xlsx $OLD
+unzip -q $OLD/template.xlsx -d $OLD
+
+cmp -s $NEW/xl/worksheets/sheet1.xml \
+       $OLD/xl/worksheets/sheet1.xml \
+  || die "CLI ($NEW) output does not match fixture ($OLD)"
+echo 'Newly generated XSLX seems to match fixture'
+rm -rf $NEW
+rm -rf $OLD
+end cli
