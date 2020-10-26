fc533ba8161f2128757f4c162eb827218781bebd
mccalluc <mccalluc@users.noreply.github.com>
2020-10-25 21:38:49 -0400

    Need to add assertions, but it runs

--- tests/test_create_xlsx.py
+++ tests/test_create_xlsx.py
@@ -9,4 +9,5 @@ def test_create_xlsx():
     schema_path = Path(__file__).parent / 'fixtures/schema.yaml'
     schema = safe_load(schema_path.read_text())
     with TemporaryDirectory() as temp_dir_name:
-        create_xlsx(schema, temp_dir_name)
+        xlsx_path = Path(temp_dir_name) / 'template.xlsx'
+        create_xlsx(schema, xlsx_path)
