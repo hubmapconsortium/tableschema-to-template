055787db87b5fbf08b418e769a87cf8159f7a8dd
mccalluc <mccalluc@users.noreply.github.com>
2020-11-30 14:36:27 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tableschema-to-template",
+    version="0.0.1",
+    author="Chuck McCallum",
+    author_email="mccallucc+tableschema@gmail.com",
+    description="Given a Frictionless Table Schema, generates an Excel template with input validation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hubmapconsortium/tableschema-to-template",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
