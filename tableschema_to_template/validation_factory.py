60c96ddab7dce272d547c5e5d50834a184a2679e
mccalluc <mccalluc@users.noreply.github.com>
2020-12-08 11:52:36 -0500

    better error messages

--- tableschema_to_template/validation_factory.py
+++ tableschema_to_template/validation_factory.py
@@ -34,18 +34,33 @@ def _get_sheet_name(field_name):
     return f'{sheet_name[:14]}...{sheet_name[-14:]}'
 
 
+def _get_enum_error_message(enum, sheet_name):
+    '''
+    >>> _get_enum_error_message(['A', 'B', 'C'], 'fake list')
+    'asdf'
+    >>> _get_enum_error_message(['A', 'B', 'C', 'D', 'E', 'F'], 'fake list')
+    'asdf'
+    '''
+    if len(enum) < 6:
+        return f'Value must be one of: {" / ".join(enum)}.'
+    return f'Value must come from {sheet_name}.'
+
+
 class EnumValidation(BaseValidation):
     def get_data_validation(self):
+        sheet_name = _get_sheet_name(self.field['name'])
+        enum_sheet = self.workbook.add_worksheet(sheet_name)
+
         enum = self.field['constraints']['enum']
-        name = _get_sheet_name(self.field['name'])
-        enum_sheet = self.workbook.add_worksheet(name)
         for i, value in enumerate(enum):
             enum_sheet.write(i, 0, value)
 
         return {
             'validate': 'list',
-            'source': f"='{name}'!$A$1:$A${len(enum)}"
+            'source': f"='{sheet_name}'!$A$1:$A${len(enum)}",
             # NOTE: OpenOffice uses "." instead of "!".
+            'error_title': 'Value must come from list',
+            'error_message': _get_enum_error_message(enum, sheet_name)
         }
 
 
@@ -57,6 +72,8 @@ class NumberValidation(BaseValidation):
             'criteria': 'between',
             'minimum': -1e+307,
             'maximum': 1e+307,
+            'error_title': 'Not a number',
+            'error_message': 'The values in this column must be numbers.'
         }
 
 
@@ -67,6 +84,8 @@ class IntegerValidation(BaseValidation):
             'criteria': 'between',
             'minimum': -2147483647,
             'maximum': 2147483647,
+            'error_title': 'Not an integer',
+            'error_message': 'The values in this column must be integers.'
         }
 
 
@@ -74,5 +93,7 @@ class BooleanValidation(BaseValidation):
     def get_data_validation(self):
         return {
             'validate': 'list',
-            'source': ['TRUE', 'FALSE']
+            'source': ['TRUE', 'FALSE'],
+            'error_title': 'Not a boolean',
+            'error_message': 'The values in this column must be "TRUE" or "FALSE".'
         }
